e941de6dbd7879e8440c6990db761839006ab0ee
Christoph Mathis <mathichr@students.zhaw.ch>
2022-11-06 20:24:19 +0100

    holder

--- app/src/main/java/ch/tenants/inkpalette/ui/grid/GridViewHolder.kt
+++ app/src/main/java/ch/tenants/inkpalette/ui/grid/GridViewHolder.kt
@@ -2,22 +2,30 @@ package ch.tenants.inkpalette.ui.grid
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ch.tenants.inkpalette.R
+import com.google.android.material.button.MaterialButton
 
-class GridViewHolder(inflater: LayoutInflater, parent: ViewGroup):
-    RecyclerView.ViewHolder(inflater.inflate(
-        R.layout.button_group,
-        parent, false)) {
-    private var nameView: TextView? = null
-    private var streetView: TextView? = null
+class GridViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(
+        inflater.inflate(
+            R.layout.button_group,
+            parent, false
+        )
+    ) {
+    private var mainButton: MaterialButton? = null
+    private var infoButton: Button? = null
+    private var upgradeButton: Button? = null
+
     init {
-        nameView = itemView.findViewById(R.id.name)
-        streetView = itemView.findViewById(R.id.street)
+        mainButton = itemView.findViewById(R.id.button_main);
+        infoButton = itemView.findViewById(R.id.button_info)
+        upgradeButton = itemView.findViewById(R.id.button_upgrade)
     }
-    fun bind(person: Person) {
-        nameView?.text = person.name
-        streetView?.text = person.street
+
+    fun bind(collectable: Collectable) {
+        mainButton?.icon = collectable.icon
     }
 }
